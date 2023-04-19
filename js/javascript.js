b5155dca290228f3c95f6e74587d0bbeb6396671
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-04-19 11:14:33 +0300

    finish hare desstop

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+/* Slider for hare section */
+const swiper = new Swiper(".slider-main-block", {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".body-main-block__arrow.swiper-button-next",
+    prevEl: ".body-main-block__arrow.swiper-button-prev",
+  },
+});
