d42513c186c8a80c560b1e208b268a88c76cd98b
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-05-12 12:53:13 +0300

    add section "deals"

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,29 @@ const swiper = new Swiper(".slider-main-block", {
     prevEl: ".body-main-block__arrow.swiper-button-prev",
   },
 });
+
+// tabs
+
+const tabNavItems = document.querySelectorAll(".tabs-deals__button");
+const tabItems = document.querySelectorAll(".item-tabs");
+
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  let currentActive = null;
+  let newActiveIndex = null;
+  if (targetElement.closest(".tabs-deals__button")) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActive = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+  }
+  targetElement.classList.add("active");
+
+  tabItems[currentActive].classList.remove("active");
+  tabItems[newActiveIndex].classList.add("active");
+});
